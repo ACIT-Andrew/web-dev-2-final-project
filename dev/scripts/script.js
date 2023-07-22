a9fa8f6e091dc69960184479e93c129e546e02af
greentea777 <117492457+greentea777@users.noreply.github.com>
2023-07-22 01:10:12 -0700

    completed header, responsive breakpoint: desktop size

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,19 @@
 // Write your JavaScript here...
+
+/* Hamburger menu toggle function */
+$(".hamburger-menu").on("click", () => {
+  $(".hamburger-menu").toggleClass("selected");
+  $(".site-header__nav").toggleClass("expanded");
+});
+
+// Define media query
+const mql = window.matchMedia("(min-width: 64.0625em)");
+
+// Add the event listener to the MediaQueryList object
+mql.addEventListener("change", removeTransition);
+function removeTransition(e) {
+  if (e.matches) {
+    $(".hamburger-menu").removeClass("selected");
+    $(".site-header__nav").removeClass("expanded");
+  }
+}
