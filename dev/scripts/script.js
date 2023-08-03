f981ec940c5bf88c68f1288fe2ea97a3b1e76114
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-08-03 12:08:54 -0700

    fixed the style of arrows of the slide and the bug from slick library related to 'resize' screen

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,23 +1,23 @@
 // Write your JavaScript here...
 
 $('.shop-by-room-or-service').slick({
+    infinite: true,
+    arrows: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: '<i class="fa-solid slick-next fa-chevron-right"></i>',
+    prevArrow: '<i class="fa-solid slick-prev fa-chevron-left"></i>',
     responsive: [
         {
             breakpoint: 1325,
             settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                infinite: true,
-                arrows: true
+                slidesToShow: 4      
             }
         },
         {
             breakpoint: 826,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                arrows: true
+                slidesToShow: 3
             }
         },
         {
@@ -29,3 +29,9 @@ $('.shop-by-room-or-service').slick({
     ]
 }
 );
+
+// fix the bug from slick whenever resize the screen from the breakpoint 'unslick'
+
+$(window).on('resize', function () {
+    $('.shop-by-room-or-service').slick('resize');
+} );
