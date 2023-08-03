092a048692a068f4b0272c7512dbfd9d3155883f
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-08-02 22:56:34 -0700

    added slick slideshow and unslick setting, but still struggling with the no show arrows

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,7 +1,31 @@
 // Write your JavaScript here...
 
-$('.testing-slide').slick({
-    dot: true,
-    arrow: true,
+$('.shop-by-room-or-service').slick({
+    responsive: [
+        {
+            breakpoint: 1325,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 826,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 450,
+            // setting the settings property
+            // to 'unslick' to destroy the slideshow
+            settings: 'unslick'
+        }
+    ]
 }
 );
