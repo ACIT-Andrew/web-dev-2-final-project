eca8308cbebd96c7d5e572ea1922026e5267a3bd
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2023-08-03 14:12:45 -0700

    Fix slick carousel position and spacing

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,56 @@
-// Write your JavaScript here...
+// if ($(window).width() < 500) {
+//   $("#images-carousel").slick("unslick");
+// } else {
+//   $("#images-carousel").slick({
+//     arrows: true,
+//     dots: false,
+//     prevArrow: '<i class="slick-prev fa-solid fa-chevron-left"></i>',
+//     nextArrow: '<i class="slick-next fa-solid fa-chevron-right"></i>',
+//     responsive: [
+//       {
+//         breakpoint: 500,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//           infinite: true,
+//         },
+//       },
+//     ],
+//   });
+// }
+
+$(window).on(
+    'resize', function(){
+        $("#images-carousel").slick('resize');
+    }
+)
+
+$("#images-carousel").slick({
+  arrows: true,
+  dots: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  prevArrow: '<i class="slick-prev fa-solid fa-chevron-left"></i>',
+  nextArrow: '<i class="slick-next fa-solid fa-chevron-right"></i>',
+  responsive: [
+    // {breakpoint: 0},
+    {
+      breakpoint: 1214,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 685,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 500,
+      settings: "unslick",
+    },
+  ],
+});
